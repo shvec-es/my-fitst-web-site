04fcaf076ddd901e3963b61dcef45aadbe7e8937
shvec-es <kotova.es.1510@gmail.com>
2021-10-06 10:44:50 +0300

    Добавляет страницы и навигацию

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const exhbs = require('express-handlebars');
+const products = require('./products.json');
+
+const PORT = process.env.PORT || 4444;
+
 const app = express();
 
-app.get('/', (req, res) => { });
-app.get('/products', (req, res) => { });
-app.get('/about', (req, res) => { });
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine('hbs', exhbs({
+    extname: 'hbs',
+}),
+);
+
+app.get('/', (req, res) => {
+    res.render('home', { cssFileName: 'home' });
+});
+app.get('/products', (req, res) => {
+    res.render('products', { products, cssFileName: 'products' });
+});
+app.get('/product/:productId', (req, res) => {
+    const product = products.find(p => p.id === req.params.productId);
+    res.render('product', { product });
+});
+app.get('/about', (req, res) => {
+    res.render('about', { cssFileName: 'about' });
+});
 
 
-app.listen(4444, () => { });
+app.listen(PORT, () => { });
